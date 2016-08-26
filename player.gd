5e8d629bc296ba5c9105177a208d97942626349d
caryoscelus <caryoscelus@gmx.com>
2016-08-26 14:36:23 +0300

    fix jitter near destination

--- player.gd
+++ player.gd
@@ -1,7 +1,12 @@
 
 extends RigidBody2D
 
+# travel speed in pixel/s
 export var speed = 200
+
+# at which distance to stop moving
+# NOTE: setting this value too low might result in jerky movement near destination
+const eps = 1.5
 
 # points in the path
 var points = []
@@ -14,8 +19,12 @@ func _fixed_process(delta):
 	points = get_node("../Navigation2D").get_simple_path(get_global_pos(), get_global_mouse_pos(), false)
 	# if the path has more than one point
 	if points.size() > 1:
-		var direction = (points[1] - get_global_pos()).normalized() # direction of movement
-		set_linear_velocity(direction*speed)
+		var distance = points[1] - get_global_pos()
+		var direction = distance.normalized() # direction of movement
+		if distance.length() > eps or points.size() > 2:
+			set_linear_velocity(direction*speed)
+		else:
+			set_linear_velocity(Vector2(0, 0)) # close enough - stop moving
 		update() # we update the node so it has to draw it self again
 
 func _draw():
